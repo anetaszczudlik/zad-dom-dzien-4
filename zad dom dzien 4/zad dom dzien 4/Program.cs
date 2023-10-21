d4ed19a34bb341b8f3bcad73942cd9fe0ff060dd
Aneta53k <aneta.szczudlik@gmail.com>
2023-10-21 20:40:05 +0200

    zadanie domowe

---
+++ zad dom dzien 4/zad dom dzien 4/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Ewa";
+var age = 33;
+string gender = "kobieta";
+
+
+if (gender == "kobieta" && age < 30)
+{ Console.WriteLine("Kobieta poniżej 30 lat."); }
+
+else if (name == "Ewa" && age == 33)
+{ Console.WriteLine("Ewa, lat 33"); }
+
+else if (gender != "kobieta" && age > 18)
+{ Console.WriteLine("Niepełnoletni mężczyzna"); }
